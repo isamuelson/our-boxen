d8329e29dd0069087f1dd458bc8031d9dcfb0f85
Bootstrapper <ido.samuelson@gmail.com>
2015-06-12 20:37:44 -0400

    Update isamuelson.pp

--- modules/people/manifests/isamuelson.pp
+++ modules/people/manifests/isamuelson.pp
@@ -2,9 +2,9 @@ class people::isamuelson (
   $my_homedir = "/Users/$::boxen_user"
 ) {
 
-  class { 'vagrantdev':
-  	vagrant_vmware_license_path => "$my_homedir/.dotfiles/vagrant/license-vmware-7.lic"
-  }
+#  class { 'vagrantdev':
+#  	vagrant_vmware_license_path => "$my_homedir/.dotfiles/vagrant/license-vmware-7.lic"
+#  }
 
   include people::isamuelson::terminal
   include people::isamuelson::apps
